d38530c479d8f4dcb620669961d4691f9eda3568
Albin Aji <albinpariyarathu@gmail.com>
2024-12-20 08:06:17 +0530

    comments moved

--- src/index.ts
+++ src/index.ts
@@ -14,8 +14,8 @@ app.use(cors());
 
 // Rate Limiting
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests
+  windowMs: 15 * 60 * 1000,
+  max: 100,
 });
 app.use(limiter);
 
